777a2b77634298ac4b1748437f8b38394ab1ba78
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-05-27 13:07:33 +0530

    dotenv config

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // God help me with this project
 const app_1 = __importDefault(require("./app"));
 const port = process.env.PORT || 3000;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 app_1.default.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // God help me with this project
 import app from "./app";
 const port = process.env.PORT || 3000;
+import dotenv from "dotenv";
+dotenv.config();
 
 app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
