4ebd2eb281e72ce9bfe2e30c9f557f9c8416f47b
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-06-03 17:33:29 +0530

    some changes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 import { populateDB } from "./populateDB";
 import { createToken } from "./token";
 import { logger } from "./logger";
-// import { generateContacts } from "./generateContacts";
 
 export { populateDB, createToken, logger };
